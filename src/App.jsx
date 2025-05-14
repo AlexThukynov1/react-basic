9f845cf666cdd92291c26cc66095667a0241994d
Tsykunov Oleksandr <50213824+AlexThukynov1@users.noreply.github.com>
2025-05-14 15:59:51 +0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css'
+import MovieCard from './components/MovieCard'
 
 function App() {
 
+const movie = {
+  title: "Movie 1",
+  release_date: "11-05-24",
+  src: ""
+}
 
   return (
       <div>
-
+        <MovieCard movie={movie}/>
       </div>
   )
 }
