17d5712636d79c4e286767f53f1814e8fd4d0d6c
Tsykunov Oleksandr <50213824+AlexThukynov1@users.noreply.github.com>
2025-05-14 17:11:16 +0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,10 @@
 import './App.css'
-import MovieCard from './components/MovieCard'
+import HomePage from './pages/HomePage'
 
 function App() {
-
-const movie = {
-  title: "Movie 1",
-  release_date: "11-05-24",
-  src: ""
-}
-const isVisible = false
-
   return (
       <div>
-        <MovieCard movie={movie}/>
-        {isVisible? <MovieCard movie={movie}/> : 'Not visible'}
+        <HomePage/>
       </div>
   )
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,40 @@
+import MovieCard from "../components/MovieCard"
+
 export default function HomePage() {
+
+const movies = [
+    {
+        id: 0,
+        title: "Movie 1",
+        release_date: "11-05-24",
+        src: ""
+    },
+    {
+        id: 1,
+        title: "Movie 1",
+        release_date: "11-05-24",
+        src: ""
+    },
+    {
+        id: 2,
+        title: "Movie 1",
+        release_date: "11-05-24",
+        src: ""
+    },
+    {
+        id: 3,
+        title: "Movie 1",
+        release_date: "11-05-24",
+        src: ""
+    }
+]
+
+
     return (
-        <div className="home-page"></div>
+        <div className="home-page">
+            <div className="movies-grid">
+                {movies.map(movie => <MovieCard movie={movie} key={movie.id}/>)}
+            </div>
+        </div>
     )
 }
